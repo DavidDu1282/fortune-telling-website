a603c88f529b42b0ab3112b2a5d56482e26635bd
David Du <david2001.du@gmail.com>
2025-01-26 10:47:20 -0800

    attempting to deply

--- src/components/Tarot/TarotReader.jsx
+++ src/components/Tarot/TarotReader.jsx
@@ -1,4 +1,4 @@
-// Main Component (TarotReader.jsx)
+// Main Tarot component (TarotReader.jsx)
 import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import SpreadSelector from "./SpreadSelector";
